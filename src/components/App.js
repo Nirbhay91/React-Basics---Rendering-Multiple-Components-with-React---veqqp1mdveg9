1057d0f58908c0a31c8677fc47e8780964ba580a
nirbhay kaushik <40763483+Nirbhay91@users.noreply.github.com>
2022-06-15 09:00:32 +0530

    commit app js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="root">
+      <h1 data-ns-test="project-name"> Moving Ticket Booking </h1>
+      <p data-ns-test="project-description">
+        Coustomer Book the movie ticket along with seat number also one person
+        book multipule seat through own id{" "}
+      </p>
+    </div>
+  );
+};
 
 export default App;
