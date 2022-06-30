0d380e72905fa67dacb72768696c209f71b50b95
nirbhay kaushik <nirbhaykaushik44@gmail.com>
2022-06-30 23:29:44 +0530

    update file

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
+    
+    return ( 
+        <div id = "main">
+      
+        <h1 data-ns-test = "project-name" > Moving Ticket Booking </h1> 
+        <p data-ns-test = "project-description" > Coustomer Book the movie ticket along with seat number also one person book multipule seat through own id </p>  
+        </div >
+    )
 }
 
 
